916582a6addca0e85b65f286fab7d9d12f4c757a
Agam Singh <agamsohi@gmail.com>
2025-07-29 19:11:46 +1000

    Also moved sober info to another file

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -4,6 +4,7 @@ import { EditDrinkModals } from "./EditDrinkModals";
 import { AddDrinkDrawer } from "./AddDrinkDrawer";
 import type { Drink } from "~/types/bac";
 import { api } from "~/trpc/react";
+import { SoberInfo } from "./SoberInfo";
 
 interface MainContentProps {
   safeBAC: {
@@ -128,22 +129,7 @@ export function MainContent({
         />
         {/* Info and controls (right on desktop) */}
         <div className="flex flex-col items-center justify-center flex-1 w-full">
-          {/* Sober time */}
-          <div className="mt-8 w-[90%] max-w-md rounded-xl bg-[#444] py-6 px-4 text-center text-lg font-medium text-[#e5e5e5] shadow md:mt-0 md:text-left">
-            {safeBAC.timeToSober > 0 
-              ? `${safeBAC.timeToSober.toFixed(2)} hours till sober`
-              : "You are sober"
-            }
-          </div>
-          {/* How long till 0.05% */}
-          <div className="mt-4 w-[90%] max-w-md rounded-xl bg-[#444] py-6 px-4 text-center text-lg font-medium text-[#e5e5e5] shadow md:text-left">
-            {safeBAC.timeToLegal > 0 
-              ? `${safeBAC.timeToLegal.toFixed(2)} hours till 0.05%`
-              : safeBAC.currentBAC <= 0.05 
-                ? "You are under 0.05%"
-                : "You are over 0.05%"
-            }
-          </div>
+          <SoberInfo safeBAC={safeBAC} />
         </div>
       </div>
 
---
+++ src/components/SoberInfo.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface SoberInfoProps {
+  safeBAC: {
+    currentBAC: number;
+    timeToSober: number;
+    timeToLegal: number;
+    isRising: boolean;
+  };
+}
+
+export function SoberInfo({ safeBAC }: SoberInfoProps) {
+  return (
+    <>
+      {/* Sober time */}
+      <div className="mt-8 w-[90%] max-w-md rounded-xl bg-[#444] py-6 px-4 text-center text-lg font-medium text-[#e5e5e5] shadow md:mt-0 md:text-left">
+        {safeBAC.timeToSober > 0
+          ? `${safeBAC.timeToSober.toFixed(2)} hours till sober`
+          : "You are sober"
+        }
+      </div>
+      {/* How long till 0.05% */}
+      <div className="mt-4 w-[90%] max-w-md rounded-xl bg-[#444] py-6 px-4 text-center text-lg font-medium text-[#e5e5e5] shadow md:text-left">
+        {safeBAC.timeToLegal > 0
+          ? `${safeBAC.timeToLegal.toFixed(2)} hours till 0.05%`
+          : safeBAC.currentBAC <= 0.05
+            ? "You are under 0.05%"
+            : "You are over 0.05%"
+        }
+      </div>
+    </>
+  );
+} 
